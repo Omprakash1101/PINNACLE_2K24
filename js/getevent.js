633f00bdb57fbbd37242cf28829ed37aebfde610
Omprakash G <108943224+Omprakash1101@users.noreply.github.com>
2024-01-24 06:59:37 +0530

    Update getevent.js

--- js/getevent.js
+++ js/getevent.js
@@ -17,7 +17,8 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var mydata = JSON.parse(this.responseText);
         str1 = str1 + "Click on the event for more details<br><br>";
-        for (const key in mydata[eventtype]) {
+        for (const key in mydata) {
+		console.log(key)
             str = str + "<option onclick='getdeatils(this)' name='eventaname'  class='theevent'   value='" + key + "'>" + key + "</option> ";
             str1 = str1 + "<div class='item'>" + key + "</div>";
         }
